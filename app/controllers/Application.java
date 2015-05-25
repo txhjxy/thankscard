cf77499530f0e65c26b8219e6fed6e5256fc0138
iTAP <iTAP@iTAP-PC15>
2015-05-25 18:05:42 +0900

    パスワード変更画面

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,8 +12,12 @@ public class Application extends Controller {
 	}
 
 	public static Result changepass(){
-		Employees emp=Employees.find.byId(session(userid));
-		
-		emp.password = Form.form(Employees.class).bindFromRequest().get();
+		Map<String, String[]> params = request().body().asFormUrlEncoded();
+		if(validation.equals(params.get("password")[0], params.get("pass")[0])){
+			Employees emp=Employees.find.byId(session(userid));
+			emp.password = Form.form(Employees.class).bindFromRequest().get();
+			return ok(Thanks.find.byId(session(userid)));
+		}
+		return ok("新しいパスワードと確認用パスワードが違います")
 	}
 }
