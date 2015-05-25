c0c623ce8301580bf07f698696055b7f5f12cc7e
User <User@H27-NotePC02>
2015-05-25 14:49:56 +0900

    make category

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,4 +26,19 @@ public class Application extends Controller {
     public static Result thanks() {
         return ok(thanks.render());
     }
+    public static Result cardview() {
+        return ok(cardview.render());
+    }
+    public static Result example() {
+        return ok(example.render());
+    }
+    public static Result createmp() {
+        return ok(createmp.render());
+    }
+    public static Result changemp() {
+        return ok(changemp.render());
+    }
+    public static Result category() {
+        return ok(category.render());
+    }
 }
