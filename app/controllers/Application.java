ec6470e4963edbc6547a9e30775221e77f8cedfb
User <User@H27-NotePC02>
2015-05-25 11:08:17 +0900

    make commonpage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,20 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import java.util.Map;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    public static Result login() {
+        return ok(login.render());
+    }
+    public static Result mainpage() {
+        return ok(mainpage.render());
+    }
+    public static Result changepass() {
+        return ok(changepass.render());
+  }
 }
