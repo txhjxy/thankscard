6959fcb5012369446878251d3674e0e4f578aa9c
iTAP <iTAP@iTAP-PC15>
2015-05-25 17:37:35 +0900

    パスワード変更まで作成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,11 +7,13 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(Thanks.find.byId(session(userid)));
-    }
+	public static Result index() {
+		return ok(Thanks.find.byId(session(userid)));
+	}
 
-    public static Result changepass(){
-
-    }
+	public static Result changepass(){
+		Employees emp=Employees.find.byId(session(userid));
+		
+		emp.password = Form.form(Employees.class).bindFromRequest().get();
+	}
 }
