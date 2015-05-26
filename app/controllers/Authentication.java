3a41b0033600b6e7a09c563f7605e68f4a24cb14
iTAP <iTAP@iTAP-PC15>
2015-05-26 11:26:28 +0900

    hennsyuu

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -5,43 +5,17 @@ import play.mvc.*;
 import views.html.authentication.*;
 public class Authentication extends Controller {
 
-	public static class Login {
-		public Integer userid;
-		public String password;
+	public static Result login() {
+        return ok(login.render(Form.form(Employees.class)));
+    }
 
-		public String validate() {
-			if (authenticate(userid, password)) {
-				return null;
-			}
-			return "ユーザーIDまたはパスワードが間違っています";
-		}
-
-		private Boolean authenticate(String userid, String password) {
-			return Employees.authenticate(userid, password);
-		}
-	}
-
-	public static Form<Login> loginForm = Form.form(Login.class);
-
-	public static Result index() {
-		if (session("login") != null) {
-			return ok("あなたは既にログインしています");
-		}
-		return ok(index.render(loginForm));
-	}
-	public static Result authenticate() {
-		Form<Login> form = loginForm.bindFromRequest();
-
-		if (form.hasErrors()) {
-			return badRequest(index.render(form));
-		} else {
-			Login login = form.get();
-			session("login", login.userid);
-			return ok(routes.Application.index.render(Thanks.find.byId(session(userid)));
-		}
-	}
-	public static Result logout() {
-	    session().clear();
-	    return redirect(routes.Authentication.index());
-	}
+    public static Result authenticate() {
+        Form<User> loginForm = Form.form(Employees.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        }
+        session().clear();
+        session("login", loginForm.get().emp_id);
+        return ok(index.render(Thanks.find.byId(session(userid)));
+    }
 }
--- app/models/Employees.java
+++ app/models/Employees.java
@@ -9,19 +9,30 @@ import javax.persistence.OneToMany;
 import play.db.ebean.Model;
 
 @Entity
- public class Employees extends Model {
+public class Employees extends Model {
 
-       @Id
-       @OneToMany(cascade=CascadeType.ALL,mappedBy="emp_id2")
-       public Integer emp_id;
+	@Id
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="emp_id2")
+	public Integer emp_id;
 
-       public String emp_name;
+	public String emp_name;
 
-       public Departments dept_id;
+	public Departments dept_id;
 
-       public String password;
+	public String password;
 
-       public Integer job_id;
+	public Integer job_id;
 
+	public String validate() {
+		if (authenticate(emp_id, password)) {
+			return null;
+		}
+		return "IDまたはパスワードが違います";
+	}
 
- }
+	private Boolean authenticate(String name, String password) {
+		User user = find.where().eq("name", name).eq("password", password).findUnique();
+		return (user != null);
+	}
+
+}
