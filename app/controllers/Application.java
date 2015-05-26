c349a5c137d4fbf45466193ca6df3d368a7aa05a
iTAP <iTAP@iTAP-PC15>
2015-05-26 13:54:15 +0900

    いろいろ手直し

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,16 +8,44 @@ import views.html.*;
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(Thanks.find.byId(session(userid)));
+		return ok(index.render());
 	}
 
 	public static Result changepass(){
-		Map<String, String[]> params = request().body().asFormUrlEncoded();
-		if(validation.equals(params.get("password")[0], params.get("pass")[0])){
-			Employees emp=Employees.find.byId(session(userid));
-			emp.password = Form.form(Employees.class).bindFromRequest().get();
-			return ok(Thanks.find.byId(session(userid)));
-		}
-		return ok("新しいパスワードと確認用パスワードが違います")
+
+		return ok("新しいパスワードと確認用パスワードが違います");
 	}
+	public static Result password() {
+		return ok(changepass.render());
+	}
+	public static Result board() {
+		return ok(board.render());
+	}
+	public static Result cardview() {
+		return ok(cardview.render());
+	}
+	public static Result category() {
+		return ok(category.render());
+	}
+	public static Result changemp() {
+		return ok(changemp.render());
+	}
+	public static Result complete() {
+		return ok(complete.render());
+	}
+	public static Result createmp() {
+		return ok(createmp.render());
+	}
+	public static Result example() {
+		return ok(example.render());
+	}
+	public static Result thanks() {
+		return ok(thanks.render());
+	}
+	public static Result detail() {
+		return ok(detail.render());
+	}
+
+
+
 }
--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -2,28 +2,17 @@ package controllers;
 
 import play.data.Form;
 import play.mvc.*;
-import views.html.authentication.*;
+
+import views.html.*;
+
 public class Authentication extends Controller {
-
+	public static Result logout() {
+		return ok(login.render());
+	}
+	public static Result authenticate() {
+		return ok(login.render());
+	}
 	public static Result login() {
-		return ok(login.render(Form.form(Employees.class)));
-		if(session(emp_id)==null){
-        	return ok(login.render(Form.form(Employees.class)));
-		}
-		return ok(index.render(Thanks.find.byId(session(emp_id)));
-    }
-
-    public static Result authenticate() {
-        Form<User> loginForm = Form.form(Employees.class).bindFromRequest();
-        if (loginForm.hasErrors()) {
-            return badRequest(login.render(loginForm));
-        }
-        session().clear();
-        session("login", loginForm.get().emp_id);
-        return ok(index.render(Thanks.find.byId(session(emp_id)));
-    }
-    public static Result thanks() {
-
-        return ok(thanks.render());
-    }
+		return ok(login.render());
+	}
 }
