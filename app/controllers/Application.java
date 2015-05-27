7603d58340a7f9e73b458e9ff95b7704698eb423
iTAP <iTAP@iTAP-PC15>
2015-05-27 17:57:20 +0900

    感謝カード作成途中

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,7 @@ import play.mvc.*;
 
 import views.html.*;
 
+import models.Employees;
 import models.Thanks;
 import models.Departments;
 import models.Categories;
@@ -15,7 +16,7 @@ import play.data.Form;
 public class Application extends Controller {
 
 	public static Result index() {
-		List<Thanks> tnk=Thanks.find.where().eq("tnk_id","2").orderBy("tnk_date desc").findList();
+		List<Thanks> tnk=Thanks.find.where().eq("tnk_id","1").orderBy("tnk_date desc").findList();
 		return ok(index.render(tnk.get(0)));
 	}
 
@@ -56,7 +57,19 @@ public class Application extends Controller {
 		return ok(thanks.render(dept,category));
 	}
 	public static Result creatthanks() {
-		Thanks newThanks = Form.form(Thanks.class).bindFromRequest().get();
+		Form<Thanks> thanksForm = Form.form(Thanks.class).bindFromRequest();
+		Thanks newThanks = new Thanks();
+		Departments dept=Departments.find.where().eq("dept_id", "thanksForm.get().dept_id").findList().get(0);
+		Employees emp=Employees.find.where().eq("emp_name", "thanksForm.get().ywk_name")
+		.eq("dept","dept_id").findList().get(0);
+		newThanks.emp2_id=emp;
+		newThanks.help_contents=thanksForm.get().help_contents;
+		newThanks.category_id=thanksForm.get().category_id;
+		newThanks.tnk_point=thanksForm.get().tnk_point;
+		newThanks.tnk_contents=thanksForm.get().tnk_contents;
+		newThanks.tnk_date=thanksForm.get().tnk_date;
+		Employees emp2=Employees.find.all().get(0);
+		newThanks.emp_id=emp2;
 		newThanks.save();
 		return ok("登録されました");
 	}
@@ -70,8 +83,7 @@ public class Application extends Controller {
             return badRequest(login.render(loginForm));
         }
         session().clear();
-        session("emp_name", loginForm.get().emp_name);
+        session("emp_id", loginForm.get().emp_name);
         return redirect(routes.Application.index());
     }
-}
 }
