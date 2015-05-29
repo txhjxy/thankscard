4fab5c2bdf43bb62b9788f6d2a115430b09a63cd
iTAP <iTAP@iTAP-PC15>
2015-05-29 16:56:53 +0900

    asd

--- app/controllers/Createmp.java
+++ app/controllers/Createmp.java
@@ -8,24 +8,29 @@ import play.data.Form;
 import views.html.*;
 
 import models.Employees;
-import models.Thanks;
 import models.Departments;
-import models.Categories;
 import java.util.*;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-public class Createmp {
+public class Createmp extends Controller {
 	@Security.Authenticated(Secured.class)
 	public static Result createmp() {
 		List<Departments> dept=Departments.find.all();
 		return ok(createmp.render(dept));
 	}
-	@Security.Authenticated(Secured.class)
-	public static Result addemp() {
-		return ok(createmp.render());
-	}
+
 	@Security.Authenticated(Secured.class)
 	public static Result complete() {
-		return ok(createmp.render());
+		Employees newEmp =new Employees();
+		String[] params = { "password","dept_id","emp_name","emp_id","job_id"};
+		DynamicForm input = Form.form();
+		input = input.bindFromRequest(params);
+		newEmp.password = input.data().get("password");
+		newEmp.dept_id =Departments.find.byId(Integer.parseInt(input.data().get("dept_id")));
+		newEmp.emp_name = input.data().get("emp_name");
+		newEmp.emp_id = input.data().get("emp_id");
+		newEmp.job_id = Integer.parseInt(input.data().get("job_id"));
+		newEmp.save();
+		return ok(complete.render(newEmp));
 	}
 }
